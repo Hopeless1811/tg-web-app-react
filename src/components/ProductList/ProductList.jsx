7252d87dd02ecf19f995d10adcff0b61bfd9028a
Hopeless1811 <vlad_mokshtadt@mail.ru>
2022-11-24 03:31:58 +0400

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://192.168.0.2:8000/web-data', {
+        fetch('http://91.105.198.171:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
