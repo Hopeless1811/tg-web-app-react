04c2b6f86ec100d2b5ddd8911eb940dfd609dbc6
Hopeless1811 <vlad_mokshtadt@mail.ru>
2022-11-24 00:30:45 +0400

    init

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import './From.css'
+import {useTelegram} from "../../hooks/useTelegram";
+
+const Form = () => {
+    const [name,setName] = useState('');
+    const [number,setNumber] = useState('');
+
+    const {tg} = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text:'Отправить данные'
+        })
+    })
+
+    useEffect(() => {
+        if(!name || !number){
+            tg.MainButton.hide();
+        }else{
+            tg.MainButton.show();
+        }
+
+    },[name,number])
+
+    const onChangeName = (e) =>{
+        setName(e.target.value);
+    }
+
+    const onChangeNumber = (e) =>{
+        setNumber(e.target.value);
+    }
+
+    return (
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+                type="text"
+                placeholder="Имя"
+                value={name}
+                onChange={onChangeName}
+            />
+            <input
+                type="text"
+                placeholder="Номер"
+                value={number}
+                onChange={onChangeNumber}
+            />
+        </div>
+    );
+};
+
+export default Form;
